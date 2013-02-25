db465d42b68c9944e7670d11aabc9a55561a596e
mluto <m@luto.at>
2013-02-25 10:31:11 +0100

    Added a simple build-script

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]
+then
+  echo "Usage: `basename $0` macosx/linx/windows [run]"
+  exit 1
+fi
+
+echo "Building for $1.."
+
+gradle build
+
+rm -r dist
+mkdir dist
+cp build/libs/Cyborg-Hippos.jar dist/Cyborg-Hippos.jar
+cp -r res dist/res
+cp -r lib/native/$1/* dist/
+
+if [ $2 == "run" ]
+then
+  echo Starting the game..
+  cd dist
+  java -jar Cyborg-Hippos.jar
+fi
